54f05096b4c030510e1ba18e5cd34a41176a5125
André Araújo <107716069+andreviearaujo@users.noreply.github.com>
2022-06-17 16:33:15 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5 // Estrutura em codição simples
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando o cinto de segurança!`)
